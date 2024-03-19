0ac20bdf1111bf47126a97a13a6be9742b94d700
Oliver <SPIJJWW@cathaypacific.com>
2024-03-19 16:35:32 +0800

    1

---
+++ my_resume/content/sume.md
@@ -0,0 +1,9 @@
+Title: Oliver Resume
+Date: 2024-03-19 15:20
+Modified: 2024-03-19 15:20
+Category: Person
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: Oliver
+
+This is my own resume.
--- my_resume/pelicanconf.py
+++ my_resume/pelicanconf.py
@@ -1,3 +1,5 @@
+THEME = 'resume'
+
 AUTHOR = 'Oliver Wang'
 SITENAME = 'Oliver Resume'
 SITEURL = ""
@@ -6,7 +8,7 @@ PATH = "content"
 
 TIMEZONE = 'Europe/Rome'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -33,3 +35,105 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+# Profile information
+NAME = 'Oliver'
+TAGLINE = '王锦涛(Wang Jin Tao)'
+PIC = 'oliver_pic.png'
+
+# sidebar links
+EMAIL = 'wjt130@outlook.com'
+PHONE = '(+86) 1234567890'
+WEBSITE = 'suheb.in'
+LINKEDIN = 'suhebk'
+GITHUB = 'suheb'
+TWITTER = '@iamsuheb'
+
+CAREER_SUMMARY = 'During five years work time, I pay attention to the cultivation of myself. Learning Java with its deep logic, get Spring original code and look into it, make sure understand projects working structure, study hard for every working chance, and also helping with each other in my working and study group. Cloud used experience as Ali cloud, Tencent cloud, Huawei cloud, and AWS is using now. Database used Mysql, Oracle and Postgresql. And I am also good at sql, python, shell, C, C++. With my understand from these, I think all develop language is a logic skin, the most importent thing is how to deal with problem as a right and simple way. Finally, in self hobits, it always table tennis, skateboarding and other sports. Keep work and life balance is good for me. Then I believe that joining the team will be a good journey of my life. Hope your opportunity. <br><br>Software Foundation:<br>1. Java and Spring Cloud project related is what I am goot at. Others like front end develop and cloud deploy is what I am familiar too.<br>2. Learning related software courses, computer software foundation, C language foundation, C++ language foundation, Matlab data algorithm and application, Verilog language foundation, microcomputer principle, etc..<br>3. Java training, programming foundation, master Java, HTML, C, C++, MyEclipse and MySql, familiar with Eclipse database, familiar with SSH, SSM framework and Web development process, know with CSS, JS, jQuery, Ajax. Have good coding habits and object-oriented programming ideas, have the spirit of continuous learning and perseverance.<br>4. Personal like AutoCAD, Solidworks, and with office software, master Photoshop, 3ds Max, Flash based applications, school used Multisim, MATLAB, know with assembly language programming, to understand the electrical and optical design.<br>'
+
+SKILLS = [
+    {
+        'title': 'JAVA',
+        'level': '100'
+    },
+    {
+        'title': 'Python',
+        'level': '95'
+    },
+    {
+        'title': 'Vue &amp; Angular &amp; React',
+        'level': '95'
+    },
+    {
+        'title': 'Javascript &amp; jQuery',
+        'level': '90'
+    },
+    {
+        'title': 'HTML5 &amp; CSS',
+        'level': '90'
+    }
+]
+
+PROJECT_INTRO = 'HongKong Cathay Pacific project, Ataff sign in management system'
+
+PROJECTS = [
+    {
+        'title': 'CPA CGO TEAM',
+        'tagline': 'HongKong Cathay Pacific project, about CGO related of my job is dealing with cargo running process such as plan, moving and estimate for air flight infomation.This project has benefited me greatly by using Java and SQL statements to complete the air flight business. Learned a lot about using AWS servers and Jenkins deploying operations. Improved my java coding level. Enhanced my sense of collaboration and worked happy with lovely colleagues.'
+    },
+    {
+        'title': 'Ataff sign in management system',
+        'tagline': 'This project completed the company attendance management system, including the functions such as: employee entry information registration, staff details and modifications, employee login, attendance, employee leave, employee late.This project is a group of 20 people together, I was responsible for the construction and function of company login, exercise our ability to unite and cooperate, let me know, do the project should not only think of themselves, to more exchanges between the team.'
+    }
+]
+
+LANGUAGES = [
+    {
+        'name': 'Chinese',
+        'description': 'Native'
+    },
+    {
+        'name': 'English',
+        'description': 'Professional'
+    },
+    {
+        'name': 'Japanese',
+        'description': 'Amateur'
+    }
+]
+
+INTERESTS = [
+    'Reading',
+    'Gaming',
+    'Python',
+    'Sport'
+]
+
+EXPERIENCES = [
+    {
+        'job_title': 'Application Developer',
+        'time': 'Sep 2021 - Present',
+        'company': 'IBM',
+        'details': 'CPA CGO TEAM Developer for cargo related system until now.'
+    },
+    {
+        'job_title': 'Full Stack Developer',
+        'time': 'Jul 2018 - Sep 2021',
+        'company': 'DL Huaxin company',
+        'details': 'Developer as Java and vue job in Dalian Huaxin company from graduation for three years before IBM.During my Huaxin work, went to Hangzhou and Fuzhou on business. Learned some local development business such as Home industry and retail industry.'
+    },
+    {
+        'job_title': 'Student',
+        'time': 'Jul 2014 - Jul 2018',
+        'company': 'Dalian Maritime University',
+        'details': 'In university, learn mechanical drawing, have factory machinery design and manufacturing experience. Part-timeworked at school library administrator to responed project management and maintenance. Participate in enterprise simulation competition competition twice.'
+    }
+]
+
+EDUCATIONS = [
+    {
+        'degree': 'Optoelectronic Information Science and Engineering',
+        'meta': 'Dalian Maritime University',
+        'time': '2014 - 2018'
+    }
+]
