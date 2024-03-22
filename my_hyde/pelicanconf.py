8ff7f6b6814322bbc385eab7e0ef7ced031866e7
Oliver <SPIJJWW@cathaypacific.com>
2024-03-22 15:59:55 +0800

    2

---
+++ my_hyde/pelicanconf.py
@@ -0,0 +1,41 @@
+THEME = 'pelican-hyde'
+AUTHOR = '王锦涛'
+SITENAME = '测试页面'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+BIO = "这是一个测试页面"
+PROFILE_IMAGE = "title.png"
+FOOTER_TEXT = "©2024 Oliver Wang"
+COLOR_THEME = "0c"
