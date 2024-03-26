7735f55f1b44f6dff513c641a28bc924ea00692f
Oliver <SPIJJWW@cathaypacific.com>
2024-03-26 16:58:57 +0800

    2

--- my_own/output/theme/js/customer.js
+++ my_own/output/theme/js/customer.js
@@ -25,18 +25,32 @@ function goToTab(tabNumber) {
     document.getElementById('tab' + tabNumber).style.display = 'block';
 }
 
-fetch('https://v1.hitokoto.cn')
-    .then(response => response.json())
-    .then(data => {
-      const hitokoto = document.getElementById('hitokoto_text')
-      const hitokotoFrom = document.getElementById('hitokoto_author')
-      hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
-      hitokotoFrom.href = 'https://hitokoto.cn/?uuid=' + data.uuid
-      hitokoto.innerText = "「 " + data.hitokoto + " 」"
-      if(data.from_who == null){
-        hitokotoFrom.innerText = "——「" + data.from + "」"
-      }else{
-        hitokotoFrom.innerText = "—— " + data.from_who + "「" + data.from + "」"
-      }
-    })
-    .catch(console.error)
+function callApi() {
+    document.getElementById('loading').style.display = 'block';
+    document.getElementById('showContent').style.display = 'none';
+    setTimeout(function() {
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('showContent').style.display = 'block';
+        const hitokoto = document.getElementById('hitokoto_text')
+        const hitokotoFrom = document.getElementById('hitokoto_author')
+        hitokoto.innerText = "「  」"
+        hitokotoFrom.innerText = "——「  」"
+        fetch('https://v1.hitokoto.cn')
+            .then(response => response.json())
+            .then(data => {
+              const hitokoto = document.getElementById('hitokoto_text')
+              const hitokotoFrom = document.getElementById('hitokoto_author')
+              hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
+              hitokotoFrom.href = 'https://hitokoto.cn/?uuid=' + data.uuid
+              hitokoto.innerText = "「 " + data.hitokoto + " 」"
+              if(data.from_who == null){
+                hitokotoFrom.innerText = "——「" + data.from + "」"
+              }else{
+                hitokotoFrom.innerText = "—— " + data.from_who + "「" + data.from + "」"
+              }
+            })
+            .catch(console.error)
+    }, 1000);
+}
+
+callApi()
